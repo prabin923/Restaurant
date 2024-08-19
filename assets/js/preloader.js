706dd3499c6c35917a45b4d9426566c50aedbe16
Prabin sharma <prabinsharma923@example.com>
2024-08-19 12:20:43 +0545

    change

---
+++ assets/js/preloader.js
@@ -0,0 +1,5 @@
+/*New! Keyboard shortcuts … Drive keyboard shortcuts have been updated to give you first-letters navigation*/
+jQuery(window).on('load', function () {
+           jQuery("body").addClass("page-loaded");
+            ("loaded")
+        });
